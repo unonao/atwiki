1403a7e031f1cee7bfbef0c2f241f9b0c8589a27
unonao <naokiuno634@gmail.com>
2018-06-13 23:25:46 +0900

    atwiki scraping

---
+++ atwiki.py
@@ -0,0 +1,71 @@
+# atwikiの更新通知機能
+# launchd を使った自動通知機能までできた
+import time
+import datetime
+import sys
+import requests
+user = "ここにユーザー名を入れる"
+passward = "ここにパスワードを入れる"
+date_now = datetime.datetime.now()
+date = date_now.strftime('%Y-%m-%d')
+
+
+def line(mes):
+    line_notify_token = 'ここにトークンを入れる'
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    #変数messageに文字列をいれて送信。 トークン名の隣に文字が来てしまうので最初に改行
+    message = '\n' + mes
+    payload = {'message': message}
+    headers = {'Authorization': 'Bearer ' + line_notify_token}
+    line_notify = requests.post(line_notify_api, data=payload, headers=headers)
+
+try:
+    # webdriverを使ってスクレイピング
+    from selenium.webdriver import Chrome, ChromeOptions
+    from selenium.webdriver.common.keys import Keys
+    options = ChromeOptions()
+    # ヘッドレスモードを有効にする（次driver = Chrome(executable_path='/chromedriver' ,options=options)の行をコメントアウトすると画面が表示される）。
+    options.add_argument('--headless')
+    # ChromeのWebDriverオブジェクトを作成する。
+    driver = Chrome(executable_path='/Users/murakaminaoki/git/atwiki/chromedriver' ,options=options)
+    driver.implicitly_wait(2)
+    driver.get("https://www65.atwiki.jp/44teck/pages/1.html")
+    driver.find_element_by_link_text("ログイン").click()
+    driver.find_element_by_name("user").send_keys(user)
+    driver.find_element_by_name("pass").send_keys(passward)
+    driver.find_element_by_name("login").click()
+    time.sleep(10)
+    day =  driver.find_elements_by_class_name("plugin_recent_day")
+    cont =  driver.find_elements_by_class_name("plugin_recent_day_div")
+    #recent = driver.find_element_by_class_name("atwiki_plugin_recent_25a1b13fb87d062d43bd6fe2d4a85a7d").text
+except:
+    print("例外が発生しました（ネットワークに繋がっているかご確認ください")
+    sys.exit()
+
+#文字列処理（過去2日分の更新情報を通知)
+his = 'updated!\n'
+for i in range(2):
+    his += '\n'
+    his += day[i].text
+    his += '\n'
+    his += cont[i].text
+    his += '\n'
+
+driver.find_element_by_link_text("ログアウト").click()
+time.sleep(6)
+driver.quit()
+
+
+
+
+#前回の結果を読み込む
+with open("/Users/murakaminaoki/git/atwiki/text.txt", encoding="utf-8") as f:
+    lines = f.read()
+    #前回の結果と同じなら処理終了
+if lines == his:
+    sys.exit()
+    #前回の結果と違うなら、ファイルを上書き
+else:
+    with open("/Users/murakaminaoki/git/atwiki/text.txt", "w", encoding="utf-8") as f:
+        f.write(his)
+line(his)
